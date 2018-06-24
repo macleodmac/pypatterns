666b5d42d3694d8975ae5b1e4c3e8fa767fde867
Jamie MacLeod <macleod.mac@gmail.com>
2018-06-24 10:06:17 +0100

    Update whitespace

--- creational/factory/example.py
+++ creational/factory/example.py
@@ -3,14 +3,11 @@ from creational.factory import factory_pythonic
 
 
 car = factory_classic.VehicleFactory.get_vehicle('car')
-
 print("Car max speed: {}".format(car.get_max_speed()))
 
 van = factory_classic.VehicleFactory.get_vehicle('van')
-
 print("Van max speed: {}".format(van.get_max_speed()))
 
 lorry = factory_pythonic.get_vehicle('lorry')
-
 print("Lorry max speed: {}".format(lorry.get_max_speed()))
 
