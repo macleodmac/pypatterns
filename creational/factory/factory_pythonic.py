068e23b1cbc9c23c51c84c014f39874c25adac71
Jamie MacLeod <macleod.mac@gmail.com>
2018-06-24 10:05:29 +0100

    Updated pythonic factory, added example

--- creational/factory/factory_pythonic.py
+++ creational/factory/factory_pythonic.py
@@ -1,15 +1,12 @@
 from creational.factory.vehicles import Car, Lorry, Van
 
 
-class VehicleFactory(object):
+def get_vehicle(vehicle_type):
+    if vehicle_type:
+        return {
+            'car': Car(),
+            'lorry': Lorry(),
+            'van': Van(),
+        }.get(vehicle_type.lower())
 
-    @staticmethod
-    def get_vehicle(vehicle_type):
-        if vehicle_type:
-            return {
-                'car': Car(),
-                'lorry': Lorry(),
-                'van': Van(),
-            }.get(vehicle_type.lower())
-
-        return None
+    return None
